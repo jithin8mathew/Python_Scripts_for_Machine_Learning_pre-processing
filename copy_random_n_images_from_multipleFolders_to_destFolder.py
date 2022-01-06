ca58ab6c7a9049734eb22ed3d83a5965ef035ef0
Jithin Mathew <jithinjosemathewoo8@gmail.com>
2022-01-06 00:06:25 -0600

    Add files via upload

---
+++ copy_random_n_images_from_multipleFolders_to_destFolder.py
@@ -0,0 +1,22 @@
+## code to copy 'n' random images from 'x' number of folder into a destination directory
+
+import random as r
+from glob import glob
+import os
+
+source_folders = ['folder1', 'folder2','folder3']
+
+
+source_memory = [z for y in source_folders for z in glob(os.path.join(y+'\\'+"*.jpg"))]
+
+dest_folder = "Destination_folder_location"
+n=2000
+i=0
+temp_list = []
+
+while i<=n:
+    src = r.choice(source_memory)
+    if src not in temp_list:
+        temp_list.append(src)
+        copy(src, os.path.join(dest_folder,str(i)+'.jpg'))
+        i+=1
